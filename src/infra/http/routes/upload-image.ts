fa93a1af058db65eaa1fff5f153f7c703498f7b4
David-Ackerman <david_pereira-16@hotmail.com>
2025-03-14 23:56:27 -0300

    integrate upload images using cloudflare r2

---
+++ src/infra/http/routes/upload-image.ts
@@ -0,0 +1,59 @@
+import { uploadImage } from '@/app/services/upload-images'
+import { db } from '@/infra/db'
+import { schema } from '@/infra/db/schemas'
+import { isRight, unwrapEither } from '@/shared/either'
+import type { FastifyInstance } from 'fastify'
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
+  server.post(
+    '/uploads',
+    {
+      schema: {
+        summary: 'Upload an image',
+        consumes: ['multipart/form-data'],
+        // body: z.object({
+        //   name: z.string(),
+        //   password: z.string().optional(),
+        // }),
+        response: {
+          201: z.null().describe('Image uploaded'),
+          400: z.object({ message: z.string() }),
+          409: z
+            .object({ messagee: z.string() })
+            .describe('Upload already exists.'),
+        },
+      },
+    },
+    async (request, reply) => {
+      const uploadedFile = await request.file({
+        limits: {
+          fileSize: 1024 * 1024 * 2,
+        },
+      })
+
+      if (!uploadedFile) {
+        return reply.status(403).send({ message: 'File is required' })
+      }
+
+      const result = await uploadImage({
+        fileName: uploadedFile.filename,
+        contentStream: uploadedFile.file,
+        contentType: uploadedFile.mimetype,
+      })
+
+      if (isRight(result)) {
+        console.log(unwrapEither(result))
+        return reply.status(201).send()
+      }
+
+      const error = unwrapEither(result)
+
+      switch (error.constructor.name) {
+        case 'InvalidFileFormat':
+          return reply.status(400).send({ message: error.message })
+      }
+    }
+  )
+}
